1f552bffcce3c9a45e8e71edd954c502263226fd
sedtyldz <ikinciyargic@gmail.com>
2024-09-03 02:33:04 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Sedat Yıldız on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
